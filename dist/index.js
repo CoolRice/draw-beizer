a7c0975ce8e72f5e4715132e01e6fbd75aeedcdb
beibo <1251243609@qq.com>
2018-08-22 23:13:43 +0800

    first version

---
+++ dist/index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function factorial(num) {
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+function Bernstein(i, n, t) {
+  // if(n < i) throw "Wrong";
+  return factorial(n) / (factorial(i) * factorial(n - i)) * Math.pow(t, i) * Math.pow(1 - t, n - i);
+}
+
+function getCurvePoint(t, points) {
+  var r = [0, 0];
+  var n = points.length - 1;
+  for (var i = 0; i <= n; i += 1) {
+    r[0] += points[i][0] * Bernstein(i, n, t);
+    r[1] += points[i][1] * Bernstein(i, n, t);
+  }
+  return r;
+}
+
+function distance(a, b) {
+  return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+}
+
+var Bezier = function () {
+  function Bezier(target, points, config) {
+    _classCallCheck(this, Bezier);
+
+    var ctx = target.getContext('2d');
+    ctx.width = config.width;
+    ctx.height = config.height;
+
+    this.target = target;
+    this.ctx = ctx;
+    this.config = config;
+    this.points = points;
+
+    this.calcStep();
+  }
+
+  _createClass(Bezier, [{
+    key: 'drawDot',
+    value: function drawDot(point) {
+      this.ctx.fillRect(point[0], point[1], 1, 1);
+    }
+  }, {
+    key: 'calcStep',
+    value: function calcStep() {
+      var totalLength = 0;
+      for (var i = 0; i < this.points.length - 1; i += 1) {
+        totalLength += distance(this.points[i], this.points[i + 1]);
+      }
+      this.step = 1 / totalLength;
+    }
+  }, {
+    key: 'draw',
+    value: function draw() {
+      for (var t = 0; t <= 1; t += this.step) {
+        var curvePoint = getCurvePoint(t, this.points);
+        this.drawDot(curvePoint, this.ctx);
+      }
+    }
+  }]);
+
+  return Bezier;
+}();
+
+// export default Bezier;
+
+var c = document.getElementById('myCanvas');
+
+var points = [[0, 0], [200, 100], [100, 200], [200, 150]];
+
+var berzier = new Bezier(c, points, {
+  width: 500,
+  height: 400
+});
+
+berzier.draw();
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function factorial(num) {
   if (num <= 1) {
     return 1;
   }
-  return num * this.factorial(num - 1);
+  return num * factorial(num - 1);
 }
 
 function Bernstein(i, n, t) {
@@ -22,22 +22,54 @@ function getCurvePoint(t, points) {
   return r;
 }
 
+function distance(a, b) {
+  return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+}
+
 class Bezier {
-  constructor(target, config) {
+  constructor(target, points, config) {
     const ctx = target.getContext('2d');
     ctx.width = config.width;
     ctx.height = config.height;
 
     this.target = target;
     this.ctx = ctx;
-    this.width = config.width;
-    this.height = config.height;
-    this.color = config.color;
+    this.config = config;
+    this.points = points;
+
+    this.calcStep();
+  }
+
+  drawDot(point) {
+    this.ctx.fillRect(point[0], point[1], 1, 1);
+  }
+
+
+  calcStep() {
+    let totalLength = 0;
+    for (let i = 0; i < this.points.length - 1; i += 1) {
+      totalLength += distance(this.points[i], this.points[i + 1]);
+    }
+    this.step = 1 / totalLength;
   }
 
   draw() {
-
+    for (let t = 0; t <= 1; t += this.step) {
+      const curvePoint = getCurvePoint(t, this.points);
+      this.drawDot(curvePoint, this.ctx);
+    }
   }
 }
 
-export default Bezier;
+// export default Bezier;
+
+const c = document.getElementById('myCanvas');
+
+const points = [[0, 0], [200, 100], [100, 200], [200, 150]];
+
+const berzier = new Bezier(c, points, {
+  width: 500,
+  height: 400,
+});
+
+berzier.draw();
